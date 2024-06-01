5035ab0ed7ec113ac3ccee08dc3dd2a467001cc0
ehabhosam14052001 <ehab.hosam2019@gmail.com>
2024-06-01 15:23:24 +0300

    middleware design pattern: zmq middlware framework

---
+++ modules/middleware/index.js
@@ -0,0 +1,7 @@
+// run zmq server
+// import "./zmq/server.js";
+// run zmq client
+// import "./zmq/client";
+
+// better run them in separate terminal windows
+// P.S. worked on node runtime and did not work on bun
